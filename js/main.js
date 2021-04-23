9d9130c09619483e1f0115b3c8a299ba23c36228
CSJ0203 <livinla44@naver.com>
2021-04-23 13:08:28 +0900

    공통모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-
-const searchEl = document.querySelector('.search'); //search클래스 안에
-const searchInputEl = searchEl.querySelector('input');//input
-
-searchEl.addEventListener('click', function(){
-    searchInputEl.focus();
-}); //searchEl에 click 이벤트 요소 추가
-
-searchInputEl.addEventListener('focus', function(){
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');  
-});   //만약에 포커스가 되면 인수 진행
-
-searchInputEl.addEventListener('blur', function(){  //포커스 해제시
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');  
-});   //만약에 포커스가 되면 인수 진행
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -145,7 +127,4 @@ spyEls.forEach(function(spyEl){
      .setClassToggle(spyEl, 'show') 
      .addTo(new ScrollMagic.Controller()); 
 });
-
-const thisYear = document.querySelector('.this-year')
-thisYear.textContent = new Date().getFullYear()
 
